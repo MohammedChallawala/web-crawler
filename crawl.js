0684b0ada083f6fd8a4ff8a43f29b914fe852431
Mohammed Challawala <mohammed.challawala2004@gmail.com>
2026-01-04 00:10:51 +0530

    Added absolute, relative and invalid link tests

--- crawl.js
+++ crawl.js
@@ -1,3 +1,37 @@
+const { link } = require('fs')
+const { JSDOM } = require('jsdom')
+
+function getURLsfromHTML(htmlBody, baseURL) {
+    const urls = []
+    const dom = new JSDOM(htmlBody)
+    const linkElements = dom.window.document.querySelectorAll('a')
+
+    for (const linkElement of linkElements) {
+        if (linkElement.href.slice(0, 1) === '/'){
+            // relative URL
+            try{
+                const urlObj = new URL(`${baseURL}${linkElement.href}`)
+                urls.push(urlObj.href)
+            }
+            catch (err){
+                console.log(`Error with relative link: ${err.message}`)
+            }
+        }
+        else{
+            try{
+                const urlObj = new URL(linkElement.href)
+                urls.push(urlObj.href)
+            }
+            catch (err){
+                console.log(`Error with absolute link: ${err.message}`)
+            }
+        }
+    }
+
+    return urls
+}
+
+
 function normalizeURL(urlString){
     const urlObj = new URL(urlString)
     const hostPath = `${urlObj.hostname}${urlObj.pathname}`
@@ -8,5 +42,6 @@ function normalizeURL(urlString){
 }
 
 module.exports = {
-    normalizeURL
+    normalizeURL,
+    getURLsfromHTML
 }
--- crawl.test.js
+++ crawl.test.js
@@ -1,4 +1,4 @@
-const { normalizeURL } = require('./crawl.js')
+const { normalizeURL, getURLsfromHTML } = require('./crawl.js')
 const {test, expect} = require('@jest/globals')
 
 // test('normalizeURL', () => {
@@ -19,5 +19,86 @@ test('normalizeURL strip slash', () => {
     const input = 'https://en.wikipedia.org/wiki/Main_Page/'
     const actual = normalizeURL(input)
     const expected = 'en.wikipedia.org/wiki/Main_Page'
+    expect(actual).toEqual(expected)
+})
+
+test('normalizeURL strip capitals', () => {
+    const input = 'https://en.WIKIPEDIA.org/wiki/Main_Page/'
+    const actual = normalizeURL(input)
+    const expected = 'en.wikipedia.org/wiki/Main_Page'
+    expect(actual).toEqual(expected)
+})
+
+test('normalizeURL strip http', () => {
+    const input = 'http://en.WIKIPEDIA.org/wiki/Main_Page/'
+    const actual = normalizeURL(input)
+    const expected = 'en.wikipedia.org/wiki/Main_Page'
+    expect(actual).toEqual(expected)
+})
+
+test('getURLsfromHTML absolute URLs', () => {
+    const inputHTMLBody = `
+    <html>
+        <body>
+            <a href = "https://en.wikipedia.org/wiki/Paracetamol">
+            The paracetamol blog
+            </a>
+        </body>
+    </html>    
+`
+    const inputBaseURL = "https://en.wikipedia.org/wiki/Paracetamol"
+    const actual = getURLsfromHTML(inputHTMLBody, inputBaseURL)
+    const expected = ["https://en.wikipedia.org/wiki/Paracetamol"]
+    expect(actual).toEqual(expected)
+})
+
+test('getURLsfromHTML relative URLs', () => {
+    const inputHTMLBody = `
+    <html>
+        <body>
+            <a href = "/Paracetamol">
+            The paracetamol blog
+            </a>
+        </body>
+    </html>    
+`
+    const inputBaseURL = "https://en.wikipedia.org/wiki"
+    const actual = getURLsfromHTML(inputHTMLBody, inputBaseURL)
+    const expected = ["https://en.wikipedia.org/wiki/Paracetamol"]
+    expect(actual).toEqual(expected)
+})
+
+test('getURLsfromHTML multiple relative URLs', () => {
+    const inputHTMLBody = `
+    <html>
+        <body>
+            <a href = "/Paracetamol">
+            The paracetamol blog
+            </a>
+            <a href = "/Ibuprofen">
+            The paracetamol blog
+            </a>
+        </body>
+    </html>    
+`
+    const inputBaseURL = "https://en.wikipedia.org/wiki"
+    const actual = getURLsfromHTML(inputHTMLBody, inputBaseURL)
+    const expected = ["https://en.wikipedia.org/wiki/Paracetamol", "https://en.wikipedia.org/wiki/Ibuprofen"]
+    expect(actual).toEqual(expected)
+})
+
+test('getURLsfromHTML invalid URLs', () => {
+    const inputHTMLBody = `
+    <html>
+        <body>
+            <a href = "invalid">
+            The paracetamol blog
+            </a>
+        </body>
+    </html>    
+`
+    const inputBaseURL = "https://en.wikipedia.org/wiki"
+    const actual = getURLsfromHTML(inputHTMLBody, inputBaseURL)
+    const expected = []
     expect(actual).toEqual(expected)
 })
